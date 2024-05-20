181915da7250a56a6828cd40632203e44ebcade9
Potti Raju <raju.p@evolutyz.com>
2024-05-20 21:47:44 +0530

    ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './componets/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './componets/employee-form/employee-form.component';
+import { LoginComponent } from './componets/login/login.component';
+import { GoogleLoginComponent } from './componets/google-login/google-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      component: EmployeeListComponent,
+      
+    },
+    {
+      path: 'employee-list',
+      component: EmployeeListComponent,
+    },
+    {
+      path: 'create-employee',
+      component: EmployeeFormComponent,
+    },
+    {
+      path: 'employee/:id',
+      component: EmployeeFormComponent,
+    },
+    {
+      path: 'login',
+      component: LoginComponent,
+    },
+    {
+      path: 'google-login',
+      component: GoogleLoginComponent,
+    },
+  ];
